042d6363bbfd4817bb6eef56859f1e5e64ea7dc6
IsmaelLangit <ismaelcruzlangit@gmail.com>
2017-06-15 15:40:20 +0800

    Modified code

--- company.php
+++ company.php
@@ -124,14 +124,14 @@ include("connect.php");
 							if($row['typeofojt'] == 'In-house' && $row['typeofcompany'] == 'Private'){
 								echo '<span class="label label-danger">Private</span> <br>';
 								echo '<span class="label label-info">In-house</span>';
-							} else if($row['typeofojt'] == 'In-house' && $row['typeofcompany'] == 'Goverment'){
-								echo '<span class="label label-success">Goverment</span> <br>';
+							} else if($row['typeofojt'] == 'In-house' && $row['typeofcompany'] == 'Government'){
+								echo '<span class="label label-success">Government</span> <br>';
 								echo '<span class="label label-info">In-house</span>';
 							} else if ($row['typeofojt'] == 'Company-based' && $row['typeofcompany'] == 'Private'){
 								echo '<span class="label label-danger">Private</span> <br>';
 								echo '<span class="label label-primary">Company-based</span>';
-							} else if ($row['typeofojt'] == 'Company-based' && $row['typeofcompany'] == 'Goverment'){
-								echo '<span class="label label-success">Goverment</span> <br>';
+							} else if ($row['typeofojt'] == 'Company-based' && $row['typeofcompany'] == 'Government'){
+								echo '<span class="label label-success">Government</span> <br>';
 								echo '<span class="label label-primary">Company-based</span>';
 							}
 			 			echo '
