dcc2ea24c4880c4f07b99c63d4da6e5117aab0db
Allen Basco <allenbascoo@gmail.com>
2017-06-20 15:32:48 +0800

    Modified code

--- add.php
+++ add.php
@@ -118,7 +118,7 @@
                 <div class="row">
                         <div class="col-md-9">
                          <form class="form-horizontal well" action="" method="post">
-                        <h2 class="z">Basic Information</h2>
+                        <h2 class="head-title titleFont">Basic Information</h2>
                         <hr class="style-four">
 
                         <div class="form-group">
