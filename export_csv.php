56782d6a9b93ab0e243449952d5720dd130421d3
IsmaelLangit <ismaelcruzlangit@gmail.com>
2017-07-07 15:06:46 +0800

    Modified exporting CSV file

--- export_csv.php
+++ export_csv.php
@@ -7,51 +7,85 @@
     $waiver           = $_POST['waiver'];
     $moa           = $_POST['moa'];
     $evaluation           = $_POST['evaluation'];
+    $courseyear           = $_POST['courseyear'];
     $selecttable           = $_POST['selecttable'];
     // filename for export
     $csv_filename = 'db_export_'.$selecttable .'_'.date('Y-m-d').'.csv';
     $where = 'WHERE 1 ORDER BY 1';
-
-if ($selecttable == "Students") {
-    $selecttable = 'idnum, concat(last_name, ", ", first_name) as name, courseyear, mobile_number, email, release_endorsement, receive_endorsement, remark_endorsement, endorsement, release_waiver, receive_waiver, remark_waiver, waiver, release_evaluation, receive_evaluation, remark_evaluation, evaluation, release_moa, receive_moa, remark_moa, moa, status';
-    $where = 'WHERE endorsement = "'.$endorsement.'" AND waiver = "'.$waiver.'" AND moa = "'.$moa.'" AND evaluation = "'.$evaluation.'"';
-} else if ($selecttable == "Company") { 
-    $selecttable = 'coid, coname, courseyear, coaddress, company_head, position,typeofcompany,release_moa,receive_moa,remark_moa,moa,status';
-    if($typeofcompany == "All") {
-        $where = 'WHERE moa = "'.$moa.'"';
+     if($endorsement == "All") {
+        $where_endorsement = '(endorsement = "yes" or endorsement = "no")';
     } else {
-        $where = 'WHERE moa = "'.$moa.'" AND typeofcompany = "'.$typeofcompany.'"';
+        $where_endorsement = ' endorsement = "'.$endorsement.'" ';
     }
-} else {
-    $selecttable = '*';
-    $where = 'WHERE endorsement = "'.$endorsement.'" AND waiver = "'.$waiver.'" AND moa = "'.$moa.'" AND evaluation = "'.$evaluation.'" AND typeofcompany = "'.$typeofcompany.'"';
-}
-// create empty variable to be filled with export data
-$csv_export = '';
-// query to get data from database
-$query = mysqli_query($connect, "SELECT ".$selecttable." FROM students NATURAL JOIN company ".$where);
-$field = mysqli_field_count($connect);
-// create line with field names
-for($i = 0; $i < $field; $i++) {
-$csv_export.= mysqli_fetch_field_direct($query, $i)->name.';';
-}
-// newline (seems to work both on Linux & Windows servers)
-$csv_export.= '
-';
-ob_end_clean();
-// loop through database query and fill export variable
-while($row = mysqli_fetch_array($query)) {
-// create line with field values
-for($i = 0; $i < $field; $i++) {
-$csv_export.= '"'.$row[mysqli_fetch_field_direct($query, $i)->name].'";';
-}
-$csv_export.= '
-';
-}
-// Export the data and prompt a csv file for download
-header("Content-type: text/x-csv");
-header("Content-Disposition: attachment; filename=".$csv_filename."");
-echo($csv_export);
+
+    if($waiver == "All") {
+       $where_waiver = '(waiver = "yes" or waiver = "no")';
+    }else {
+        $where_waiver = ' waiver = "'.$waiver.'" ';
+    }
+
+    if($moa == "All") {
+        $where_moa = '(moa = "yes" or moa = "no")';
+    }else {
+        $where_moa = ' moa = "'.$moa.'" ';
+    }
+
+    if($evaluation == "All") {
+       $where_evaluation = '(evaluation = "yes" or evaluation = "no")';
+    }else {
+        $where_evaluation = ' evaluation = "'.$evaluation.'" ';
+    }
+
+    if($typeofcompany == "All") {
+       $where_typeofcompany = '(typeofcompany = "Private" or evaluation = "Government")';
+    }else {
+        $where_typeofcompany = ' typeofcompany = "'.$typeofcompany.'" ';
+    }
+
+    if($courseyear == "All") {
+       $where_courseyear = 'courseyear LIKE "BS%"';
+    }else {
+        $where_courseyear = 'courseyear LIKE "'.$courseyear.'%"';
+    }
+
+
+
+    if ($selecttable == "Students") {
+        $selecttable = 'idnum, concat(last_name, ", ", first_name) as name, courseyear, mobile_number, email, release_endorsement, receive_endorsement, remark_endorsement, endorsement, release_waiver, receive_waiver, remark_waiver, waiver, release_evaluation, receive_evaluation, remark_evaluation, evaluation, release_moa, receive_moa, remark_moa, moa, status, coname, typeofcompany';
+        $where = 'WHERE '.$where_endorsement.' AND '.$where_waiver.' AND '.$where_moa.' AND '.$where_evaluation.' AND '.$where_typeofcompany.' AND '.$where_courseyear;      
+    } else if ($selecttable == "Company") { 
+        $selecttable = 'count(students.coid) as "countstudent",coid, coname, coaddress, company_head, position, typeofcompany';
+        $where = 'WHERE coname != "No Company AND '.$where_moa.' AND '.$where_typeofcompany;
+    } else {
+        $selecttable = '*';
+        $where = 'WHERE '.$where_endorsement.' AND '.$where_waiver.' AND '.$where_moa.' AND '.$where_evaluation.' AND '.$where_typeofcompany.' AND '.$where_courseyear; 
+    }
+    // create empty variable to be filled with export data
+    $csv_export = '';
+    // query to get data from database
+    $query = mysqli_query($connect, "SELECT ".$selecttable." FROM students NATURAL JOIN company ".$where);
+    $field = mysqli_field_count($connect);
+    // create line with field names
+    for($i = 0; $i < $field; $i++) {
+    $csv_export.= mysqli_fetch_field_direct($query, $i)->name.';';
+    }
+    // newline (seems to work both on Linux & Windows servers)
+    $csv_export.= '
+    ';
+    ob_end_clean();
+    // loop through database query and fill export variable
+    while($row = mysqli_fetch_array($query)) {
+    // create line with field values
+    for($i = 0; $i < $field; $i++) {
+    $csv_export.= '"'.$row[mysqli_fetch_field_direct($query, $i)->name].'";';
+    }
+    $csv_export.= '
+    ';
+    }
+    // Export the data and prompt a csv file for download
+    header("Content-type: text/x-csv");
+    header("Content-Disposition: attachment; filename=".$csv_filename."");
+    echo($csv_export);
 }
 ?>
 
@@ -106,30 +140,43 @@ echo($csv_export);
                 <a href="javascript:" id="return-to-top"><i class="glyphicon glyphicon-chevron-up"></i></a>
                 <form class="form-inline" method="post">
                     <h2 class="head-title titleFont">Tables</h2>
-        			 <select name="selecttable" class="form-control touch">
-        		        <?php $selecttable = (isset($_POST['selecttable']) ? strtolower($_POST['selecttable']) : NULL);  ?>
-        		        <option value="Students" <?php if($selecttable == 'students'){ echo 'selected'; } ?>>Students</option>
-        		        <option value="Company" <?php if($selecttable == 'company'){ echo 'selected'; } ?>>Company</option>
-        		        <option value="Students+Company" <?php if($selecttable == 'students+Company'){ echo 'selected'; } ?>>Students + Company</option>
-        		    </select>
-        			<h2 class="head-title titleFont">Student Information</h2>
-                    <hr class="style-four">
-        			<input type="hidden" name="endorsement" value="no">                                          
-        		    <input type="checkbox" name="endorsement" value="yes" checked><span class="space"></span>Endorsement<br> 
-        		    <input type="hidden" name="waiver" value="no">                                          
-        		    <input type="checkbox" name="waiver" value="yes" checked><span class="space"></span>Waiver<br> 
-        		    <input type="hidden" name="evaluation" value="no">                                          
-        		    <input type="checkbox" name="evaluation" value="yes" checked><span class="space"></span>Evaluation<br> 
-        			<h2 class="head-title titleFont">Company Information</h2>
-                    <hr class="style-four">
-                    <input type="hidden" name="moa" value="no">                                          
-                    <input type="checkbox" name="moa" value="yes" checked><span class="space"></span>Memorandum of Agreement<br> 
-                    <h4> TYPE </h4>
-                    <input type="radio" name="typeofcompany" value="All" required> All<br>    
-        			<input type="radio" name="typeofcompany" value="Private" required> Private<br>                                          
-        			<input type="radio" name="typeofcompany" value="Government" required> Government<br>
-        			<input type="submit" name="export" class="btn btn-md btn-success paddingTopSlight" value="Export data">
-        		</form>
+                     <select name="selecttable" class="form-control touch">
+                        <?php $selecttable = (isset($_POST['selecttable']) ? strtolower($_POST['selecttable']) : NULL);  ?>
+                        <option value="Students" <?php if($selecttable == 'students'){ echo 'selected'; } ?>>Students</option>
+                        <option value="Company" <?php if($selecttable == 'company'){ echo 'selected'; } ?>>Company</option>
+                        <option value="Students+Company" <?php if($selecttable == 'students+Company'){ echo 'selected'; } ?>>Students + Company</option>
+                    </select>
+                        <h2 class="head-title titleFont">Student Information</h2>
+                        <hr class="style-four">
+                        <h4> COURSE </h4>
+                        <input type="radio" name="courseyear" value="All" required checked> All<br>    
+                        <input type="radio" name="courseyear" value="BSCS" required> BSCS<br>                                          
+                        <input type="radio" name="courseyear" value="BSIT" required> BSIT<br>
+                        <h4> ENDORSEMENT </h4>
+                        <input type="radio" name="endorsement" value="All" required checked> All<br>    
+                        <input type="radio" name="endorsement" value="yes" required> Submitted<br>                                          
+                        <input type="radio" name="endorsement" value="no" required> Not Submitted<br>
+                        <h4> WAIVER </h4>                                  
+                        <input type="radio" name="waiver" value="All" required checked> All<br>    
+                        <input type="radio" name="waiver" value="yes" required> Submitted<br>                                          
+                        <input type="radio" name="waiver" value="no" required> Not Submitted<br>
+                        <h4> EVALUATION </h4>                                     
+                        <input type="radio" name="evaluation" value="All" required checked> All<br>    
+                        <input type="radio" name="evaluation" value="yes" required> Submitted<br>                                          
+                        <input type="radio" name="evaluation" value="no" required> Not Submitted<br>
+
+                        <h2 class="head-title titleFont">Company Information</h2>
+                        <hr class="style-four">
+                        <h4> MEMORANDUM OF AGREEMENT </h4>  
+                        <input type="radio" name="moa" value="All" required checked> All<br>    
+                        <input type="radio" name="moa" value="yes" required> Submitted<br>                                          
+                        <input type="radio" name="moa" value="no" required> Not Submitted<br> 
+                        <h4> TYPE </h4>
+                        <input type="radio" name="typeofcompany" value="All" required checked> All<br>    
+                        <input type="radio" name="typeofcompany" value="Private" required> Private<br>                                          
+                        <input type="radio" name="typeofcompany" value="Government" required> Government<br>
+                        <input type="submit" name="export" class="btn btn-md btn-success paddingTopSlight" value="Export data">
+                </form>
             </div>
         </div>
     </section>
