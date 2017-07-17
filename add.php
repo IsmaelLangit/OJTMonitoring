af1188d87f202d681e55fa021305b384bdb5febb
Allen Basco <allenbascoo@gmail.com>
2017-07-17 11:59:39 +0800

    Added transitions in the bootstrap-notify

--- add.php
+++ add.php
@@ -103,11 +103,10 @@
                 
                     $insert = mysqli_query($connect, "INSERT INTO students(idnum, last_name, first_name, courseyear, mobile_number, email, release_endorsement, receive_endorsement, remark_endorsement, endorsement, release_waiver, receive_waiver, remark_waiver, waiver, release_evaluation, receive_evaluation, remark_evaluation, evaluation, coid, status, ad_id) VALUES('$idnum','$last_name', '$first_name','$courseyear','$mobile_number','$email', '$release_endorsement', '$receive_endorsement', '$remark_endorsement', '$endorsement', '$release_waiver', '$receive_waiver', '$remark_waiver', '$waiver', '$release_evaluation', '$receive_evaluation', '$remark_evaluation','$evaluation','$coid','$status','$ad_id')") or die('Error: ' . mysqli_error($connect));
 
-                        echo '<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span class = "fa fa-check-circle"></span> Success!</strong> You have successfully added a student.  <a href="index.php" class="alert-link"><span class="fa fa-arrow-circle-left"></span> Go back to list of students.</a>.
-                                </div>';
+                       echo '<script type="text/javascript">success()</script>';
                     
                 } else {
-                    echo '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><span class="fa fa-exclamation-circle"></span> The student you are adding <strong> already exists in the database.</strong></div>';
+                    echo '<script type="text/javascript">error()</script>';
                     }
                 
             }
@@ -392,7 +391,7 @@
     })
     </script>
 
-    <script>
+    <script type="text/javascript">
         $(function success(){
           $(".btn").on("click",function(){
             $.notify({
@@ -402,8 +401,8 @@
             },{
               type: 'success',
               animate: {
-                    enter: 'animated fadeInDown',
-                    exit: 'animated fadeOutUp'
+                    enter: 'animated fadeInUp',
+                    exit: 'animated fadeOutRight'
               },
               placement: {
                 from: "top",
@@ -421,7 +420,7 @@
         });
     </script>
 
-    <script>
+    <script type="text/javascript">
         $(function error(){
           $(".btn").on("click",function(){
             $.notify({
@@ -430,8 +429,8 @@
             },{
               type: 'danger',
               animate: {
-                    enter: 'animated fadeInDown',
-                    exit: 'animated fadeOutUp'
+                    enter: 'animated fadeInUp',
+                    exit: 'animated fadeOutRight'
               },
               placement: {
                 from: "top",
