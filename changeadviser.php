1de7eb47953238d61964655e2f84258e22dc309e
ismaellangit <ismaelcruzlangit@gmail.com>
2017-07-18 11:37:34 +0800

    Modified the page for assigning an adviser for students

--- changeadviser.php
+++ changeadviser.php
@@ -321,7 +321,7 @@ include("connect.php");
           table = document.getElementById("myTable2");
           tr = table.getElementsByTagName("tr");
           for (i = 0; i < tr.length; i++) {
-            var td1 = tr[i].getElementsByTagName("td")[1];
+            var td1 = tr[i].getElementsByTagName("td")[0];
             if (td1) {
               if (td1.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
